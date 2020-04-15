b2c25a5cd193c51360a5f539f3853cb2f7a7a613
Michał Pawelski <kontakt@proapi.eu>
2020-04-16 00:45:46 +0200

    feat(handler): Add different response message

--- handler.py
+++ handler.py
@@ -11,7 +11,7 @@ def hello(event, context):
     }
 
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "This is just a test message as a response",
         "input": event
     }
 
@@ -23,11 +23,3 @@ def hello(event, context):
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
